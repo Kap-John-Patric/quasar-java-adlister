29823de138a64c3e597c9748e75d4585f9f1d2ad
john alejandro <john.e.alejandro01@gmail.com>
2021-12-09 16:47:45 -0600

    Saving current work

--- src/main/java/com/codeup/adlister/controllers/ArtistProfileServlet.java
+++ src/main/java/com/codeup/adlister/controllers/ArtistProfileServlet.java
@@ -1,21 +1,21 @@
-package com.codeup.adlister.controllers;
-
-import javax.servlet.*;
-import javax.servlet.http.*;
-import javax.servlet.annotation.*;
-import java.io.IOException;
-
-@WebServlet(name = "ArtistProfileServlet", urlPatterns = "ads/artistprofile")
-public class ArtistProfileServlet extends HttpServlet {
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("WEB-INF/ads/artistprofile.jsp").forward(request, response);
-
-
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-    }
-}
+//package com.codeup.adlister.controllers;
+//
+//import javax.servlet.ServletException;
+//import javax.servlet.http.*;
+//import javax.servlet.annotation.*;
+//import java.io.IOException;
+//
+//@WebServlet(name = "controllers.ArtistProfileServlet", urlPatterns = "/ads/artist_profile")
+//public class ArtistProfileServlet extends HttpServlet {
+//    @Override
+//    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//        request.getRequestDispatcher("WEB-INF/ads/artist_profile.jsp").forward(request, response);
+//
+//
+//    }
+//
+////    @Override
+////    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+////
+////    }
+//}
