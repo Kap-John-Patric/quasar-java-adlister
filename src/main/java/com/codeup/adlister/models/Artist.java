7cb2c200352c7961d4c569c8dade9efb506bd9fd
Kapena Magallan <kapena.a.m.magallan@gmail.com>
2021-12-10 14:04:35 -0600

    changed artist register urlPath, added more classes

---
+++ src/main/java/com/codeup/adlister/models/Artist.java
@@ -0,0 +1,49 @@
+package com.codeup.adlister.models;
+
+public class Artist {
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String bandName;
+
+    public Artist(){}
+
+    public Artist(long id, String firstName, String lastName, String bandName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.bandName = bandName;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getBandName() {
+        return bandName;
+    }
+
+    public void setBandName(String bandName) {
+        this.bandName = bandName;
+    }
+}
