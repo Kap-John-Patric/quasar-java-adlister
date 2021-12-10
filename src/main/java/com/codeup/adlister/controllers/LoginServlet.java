0ad11c49b6d1e34f6fe249dfa49d4fe13e678972
patric noriega <patricnoriega@gmail.com>
2021-12-10 11:38:36 -0600

    checking status on password check

--- src/main/java/com/codeup/adlister/controllers/LoginServlet.java
+++ src/main/java/com/codeup/adlister/controllers/LoginServlet.java
@@ -1,18 +1,14 @@
 package com.codeup.adlister.controllers;
-
 import com.codeup.adlister.dao.DaoFactory;
 import com.codeup.adlister.models.User;
-import com.mysql.cj.jdbc.Driver;
-import com.sun.jdi.connect.Connector;
-import org.mindrot.jbcrypt.BCrypt;
-
+import com.codeup.adlister.util.Password;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.sql.*;
 
 @WebServlet(name = "controllers.LoginServlet", urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
@@ -24,7 +20,7 @@ public class LoginServlet extends HttpServlet {
         request.getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
     }
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
         User user = DaoFactory.getUsersDao().findByUsername(username);
@@ -32,32 +28,21 @@ public class LoginServlet extends HttpServlet {
             response.sendRedirect("/login");
             return;
         }
+//        HttpSession currentSession = request.getSession();
 
-        boolean validAttempt = password.equals(user.getPassword());
-
+        boolean validAttempt = Password.check(password, user.getPassword());
         if (validAttempt) {
+//            currentSession.setAttribute("validAttempt", true);
             request.getSession().setAttribute("user", user);
             response.sendRedirect("/profile");
+//            response.getWriter().println("Successful login");
         } else {
             response.sendRedirect("/login");
+//            response.getWriter().println("Invalid login");
+
         }
+        request.getRequestDispatcher("/WEB-INF/profile.jsp").forward(request, response);
 
-
-        String hash = BCrypt.hashpw(password, BCrypt.gensalt());
-        boolean isAuthenticated = BCrypt.checkpw(password, hash);
-        try {
-            connection = Connector.ConnectDb();
-            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM users WHERE username= '" + username + "'");
-
-            ResultSet resultSet = preparedStatement.executeQuery();
-            while (resultSet.next()) {
-                if (BCrypt.checkpw(password, resultSet.getString("Password"))) {
-
-
-                }
-            }
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
     }
 }
+
