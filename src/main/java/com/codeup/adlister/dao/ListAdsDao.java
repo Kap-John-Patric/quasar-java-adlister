7437e328d0aaaf80b60a521967358b7858b0b282
Kapena Magallan <kapena.a.m.magallan@gmail.com>
2021-12-13 16:25:26 -0600

    Working in ListAdsDao

--- src/main/java/com/codeup/adlister/dao/ListAdsDao.java
+++ src/main/java/com/codeup/adlister/dao/ListAdsDao.java
@@ -1,55 +1,59 @@
 //package com.codeup.adlister.dao;
-//
-//import com.codeup.adlister.models.Ad;
+//import com.codeup.adlister.models.Concert;
 //
 //import java.util.ArrayList;
 //import java.util.List;
 //
 //public class ListAdsDao implements Concerts {
-//    private List<Concerts> ads;
+//    private List<Concert> concerts;
 //
-//    public List<Ad> all() {
-//        if (ads == null) {
-//            ads = generateAds();
+//    public List<Concert> all() {
+//        if (concerts == null) {
+//            generateAds();
 //        }
-//        return ads;
+//        return concerts;
 //    }
 //
-//    public Long insert(Ad ad) {
+//    @Override
+//    public List<Concert> searchAds(String name) {
+//        return null;
+//    }
+//
+//    public Long insert(Concert concerts) {
 //        // make sure we have ads
-//        if (ads == null) {
-//            ads = generateAds();
+//        if (concerts == null) {
+//            generateAds();
 //        }
 //        // we'll assign an "id" here based on the size of the ads list
 //        // really the dao would handle this
-//        ad.setId((long) ads.size());
-//        ads.add(ad);
-//        return ad.getId();
+//        Concert.setId((long) concert.size());
+//        concerts.add((Ad) concert);
+//        return concert.getId();
 //    }
 //
-//    private List<Ad> generateAds() {
-//        List<Ad> ads = new ArrayList<>();
-//        ads.add(new Ad(
+//    private List<Concert> generateAds() {
+//        List<Concert> ads = new ArrayList<>();
+//        ads.add(new Concert(
 //            1,
-//            1,
+//            "Whiskey",
 //            "playstation for sale",
 //            "This is a slightly used playstation"
 //        ));
-//        ads.add(new Ad(
+//        ads.add(new Concert(
 //            2,
-//            1,
+//            "Jack Daniels",
 //            "Super Nintendo",
 //            "Get your game on with this old-school classic!"
 //        ));
-//        ads.add(new Ad(
+//        ads.add(new Concert(
 //            3,
-//            2,
+//            "Steve Smith",
 //            "Junior Java Developer Position",
 //            "Minimum 7 years of experience required. You will be working in the scripting language for Java, JavaScript"
 //        ));
-//        ads.add(new Ad(
+//        ads.add(new Concert(
 //            4,
-//            2,
+//            "John Smith",
 //            "JavaScript Developer needed",
 //            "Must have strong Java skills"
 //        ));
